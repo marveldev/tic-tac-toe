6cfa45e0020b82a00383546077e0b25ad7eaab5b
marveldev <marvellousalika48@gmail.com>
2021-08-29 19:27:37 +0100

    add functionality to checkWinner. add functionality to checkIfTie. rename doStuff function to addSquareValue.

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -1,12 +1,43 @@
+import { useEffect, useState } from 'react'
+import { patterns } from './patterns'
 import './ticTacToe.scss'
-import { useState } from 'react'
 
 const TicTacToe = () => {
   const [currentPlayer, setCurrentPlayer] = useState('x')
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""])
+  const [noOfWIns, setNoOfWins] = useState({playerX: 0, playerO: 0})
   
-  const doStuff = index => {
-    if (!squares[index]) {
+  const checkWinner = () => {
+    for (let index = 0; index < patterns.length; index++) {
+      const [a, b, c] = patterns[index]
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
+      }
+    }
+    
+    return null
+  }
+  
+  const winner = checkWinner()
+  
+  const checkIfTie = () => {
+    let squareIsFilled = true
+    for (let index = 0; index < squares.length; index++) {
+      if (squares[index] === "") squareIsFilled = false
+    }
+  
+    if (squareIsFilled && !winner) {
+        console.log('GAME IS TIE')
+    }
+  }
+  
+  useEffect(() => {
+    console.log(winner)
+    checkIfTie()
+  },[checkIfTie, winner])
+  
+  const addSquareValue = (square, index) => {
+    if (square === "") {
       const mutableData = [...squares]
       mutableData[index] = currentPlayer
       setSquares(mutableData)
@@ -27,9 +58,9 @@ const TicTacToe = () => {
             }
           </div>
           <div>
-            <span>1</span>
+            <span>{noOfWIns.playerX}</span>
             <span> : </span>
-            <span>0</span>
+            <span>{noOfWIns.playerO}</span>
           </div>
           <div>
             <p>Player 2</p>
@@ -41,7 +72,7 @@ const TicTacToe = () => {
         </div>
         <div className={"content"}>
           {squares?.map((square, index) =>
-            <div key={index} onClick={() => doStuff(index)}>{square}</div>
+            <div key={index} onClick={() => addSquareValue(square, index)}>{square}</div>
           )}
         </div>
       </div>
